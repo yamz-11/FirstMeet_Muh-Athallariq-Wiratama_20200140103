30da9496f4a44f7498b517920c3981d60a41ad24
yamz-11 <wiratamajrz@gmail.com>
2022-10-18 14:47:06 +0700

    cemilan tugas koneksi db

---
+++ src/main/java/cemilan/koneksidb/KoneksidbApplication.java
@@ -0,0 +1,13 @@
+package cemilan.koneksidb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoneksidbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoneksidbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cemilan/koneksidb/KoneksidbApplicationTests.java
@@ -0,0 +1,13 @@
+package cemilan.koneksidb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoneksidbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
